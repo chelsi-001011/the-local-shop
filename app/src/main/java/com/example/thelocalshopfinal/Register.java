0e231b19e26d8fba12064f3b7331e204cc620ded
Chelsi Rawat <chelzee.rawat@yahoo.in>
2021-05-20 13:13:57 +0530

    Created two types of users

--- app/src/main/java/com/example/thelocalshopfinal/Register.java
+++ app/src/main/java/com/example/thelocalshopfinal/Register.java
@@ -2,91 +2,52 @@ package com.example.thelocalshopfinal;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
 public class Register extends AppCompatActivity {
-    EditText mFullName, mEmail, mPassword, mPhone;
-    Button mRegisterBtn;
-    TextView mLoginBtn;
-    FirebaseAuth fAuth;
-    ProgressBar progressBar;
+
+    Button customerBtn,storeBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mFullName = findViewById(R.id.name);
-        mEmail = findViewById(R.id.emailAddress);
-        mPassword = findViewById(R.id.password);
-        mPhone = findViewById(R.id.phoneNumber);
-        mRegisterBtn = findViewById(R.id.registerButton);
-        mLoginBtn = findViewById(R.id.alreadyRegisteredLogin);
+        customerBtn = findViewById(R.id.customerBtn);
+        storeBtn = findViewById(R.id.storeBtn);
 
-        fAuth= FirebaseAuth.getInstance();
-        progressBar = findViewById(R.id.progressBar);
-
-        if(fAuth.getCurrentUser() !=null){
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            finish();
-        }
-
-        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+        customerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String email = mEmail.getText().toString().trim();
-                String password = mPassword.getText().toString().trim();
+            public void onClick(View view) {
+                try {
+                    startActivity(new Intent(Register.this, Register_Customer.class));
+                    finish();
 
-
-                if(TextUtils.isEmpty(email)){
-                    mEmail.setError("EMAIL IS REQUIRED.");
-                    return;
                 }
-
-                if(TextUtils.isEmpty(password)){
-                    mPassword.setError("PASSWORD IS REQUIRED.");
+                catch(Exception e){
+                    Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        storeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
 
-                if(password.length()<6){
-                    mPassword.setError("PASSWORD MUST BE 6 CHARACTERS ATLEAST.");
+
+                    startActivity(new Intent(Register.this, Register_Store.class));
+                    finish();
+
                 }
-
-                progressBar.setVisibility(View.VISIBLE);
-
-                //register the user in firebase
-
-                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        }else{
-                            Toast.makeText(Register.this,"ERROR! "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                catch(Exception e){
+                    Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),Login.class));
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/example/thelocalshopfinal/Register_Store.java
@@ -0,0 +1,117 @@
+package com.example.thelocalshopfinal;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.text.TextUtils;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ProgressBar;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.OnSuccessListener;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.firestore.DocumentReference;
+        import com.google.firebase.firestore.FirebaseFirestore;
+
+        import org.w3c.dom.Document;
+
+        import java.util.HashMap;
+        import java.util.Map;
+
+public class Register_Store extends AppCompatActivity {
+    EditText mFullName, mEmail, mPassword, mPhone;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+    FirebaseFirestore fstore;
+    String userID;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__store);
+
+        mFullName = findViewById(R.id.name);
+        mEmail = findViewById(R.id.emailAddress);
+        mPassword = findViewById(R.id.password);
+        mPhone = findViewById(R.id.phoneNumber);
+        mRegisterBtn = findViewById(R.id.registerButton);
+        mLoginBtn = findViewById(R.id.alreadyRegisteredLogin);
+
+        fAuth= FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String fullName = mFullName.getText().toString();
+                String phone = mPhone.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("EMAIL IS REQUIRED.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("PASSWORD IS REQUIRED.");
+                }
+
+                if(password.length()<6){
+                    mPassword.setError("PASSWORD MUST BE 6 CHARACTERS ATLEAST.");
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //register the user in firebase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register_Store.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fstore.collection("store").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("StoreName",fullName);
+                            user.put("email",email);
+                            user.put("phone",phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+//                                    Log.d("TAG","onSuccess: user profile is created for "+ userID);
+                                }
+                            });
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(Register_Store.this,"ERROR! "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+    }
+}
